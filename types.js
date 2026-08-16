dce97b1d9e00b663583583c5f45e1f932e9d764e
Wang-Yile <bluewindde@163.com>
2026-08-16 19:46:17 +0800

    chore(types): 类型注释

---
+++ types.js
@@ -0,0 +1,62 @@
+/**
+ * @typedef {{
+ *   luogu: number,
+ * }} Account
+ * @typedef {{name: string, accounts: Account[]}} Group
+ * @typedef {[Set<number>, Set<number>]} Situation
+ */
+
+/**
+ * @typedef {{
+ *   type: string,
+ *   title: string,
+ *   difficulty: number,
+ *   pid: string,
+ * }} LuoguProblem
+ * @typedef {{
+ *   pid: string,
+ *   type: string,
+ *   name: string,
+ *   difficulty: string,
+ *   submitted: boolean,
+ *   accepted: boolean,
+ *   tags: number[],
+ *   totalSubmit: number,
+ *   totalAccepted: number,
+ *   flag: number,
+ *   provider: LuoguProfile
+ * }} LuoguProblemDetail
+ */
+
+/**
+ * @typedef {{
+ *   id: number,
+ *   name: string,
+ *   type: number,
+ *   provider: {
+ *     id: number,
+ *     name: string,
+ *     isPremium: boolean
+ *   },
+ *   createTime: number,
+ *   deadline: number | null,
+ *   problemCount: number,
+ *   markCount: number,
+ *   marked: boolean,
+ *   description: string,
+ *   problems: LuoguProblemDetail[]
+ * }} LuoguTraining
+ */
+
+/**
+ * @typedef {{
+ *   name: string,
+ *   privacy: boolean
+ * }} LuoguProfileNew
+ * @typedef {{
+ *   passed: LuoguProblem[],
+ *   submitted: LuoguProblem[],
+ *   name: string,
+ *   privacy: boolean
+ * }} LuoguPracticeNew
+ */
