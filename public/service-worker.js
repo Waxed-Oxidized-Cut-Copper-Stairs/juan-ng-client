aedf7af0eeb8d4360bff650040d18a2bcdffe62c
Wang-Yile <bluewindde@163.com>
2026-08-16 19:56:55 +0800

    feat(sw): 添加 Sercie Worker 及对应通信协议

---
+++ public/service-worker.js
@@ -0,0 +1,127 @@
+const proxyURL = "http://127.0.0.1:6969";
+console.log(`服务端地址 ${proxyURL}`);
+
+const usersPromise = (async () => {
+    const resp = await fetch(`${proxyURL}/data`);
+    if (!resp.ok) {
+        throw new Error("无法获取数据，请检查服务端是否正常启动 :(");
+    }
+    return await resp.json();
+})();
+
+import { error } from "./lib/core.js";
+
+/** @type {Promise<void> | null} */
+let offscreenPromise = Promise.resolve();
+async function createOffscreen() {
+    offscreenPromise = offscreenPromise.then(async () => {
+        if (await chrome.offscreen.hasDocument()) return;
+        await chrome.offscreen.createDocument({
+            url: "offscreen.html",
+            reasons: ["WORKERS"],
+            justification: "需要常驻 Worker 执行爬取任务",
+        });
+    });
+    return offscreenPromise;
+}
+async function killOffscreen() {
+    offscreenPromise = offscreenPromise.then(async () => {
+        if (!await chrome.offscreen.hasDocument()) return;
+        await chrome.offscreen.closeDocument();
+    });
+    return offscreenPromise;
+}
+
+/**
+ * @param {Promise} promise
+ * @param {(response?: any) => void} sendResponse
+ */
+async function sendResponseWrapper(promise, sendResponse) {
+    return promise.catch(err => {
+        error(err);
+        try { sendResponse(); }
+        catch (err) { error("无法发送响应", err); }
+    });
+}
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    const { dst, type, data } = message;
+    if (dst !== "sw") return;
+    switch (type) {
+        case "query-users": {
+            usersPromise.then((users) => sendResponse(users), sendResponse);
+            return true;
+        }
+        case "query-pid":
+        case "query-profile":
+        case "query-progress": {
+            sendResponseWrapper(createOffscreen().then(async () => {
+                return chrome.runtime.sendMessage({ dst: "offscreen", type, data });
+            }).then(async resp => {
+                sendResponse(resp);
+            }), sendResponse);
+            return true;
+        }
+        case "query-uid": {
+            sendResponseWrapper(chrome.cookies.get({ url: "https://*.luogu.com.cn", name: "_uid" }).then(
+                cookies => {
+                    if (cookies) {
+                        const uid = Number.parseInt(cookies.value);
+                        if (!isNaN(uid)) sendResponse(uid);
+                        else sendResponse();
+                    } else sendResponse();
+                }
+            ), sendResponse);
+            return true;
+        }
+        case "route-to-active-tabs": {
+            chrome.tabs.query({
+                active: true, highlighted: true,
+                url: [
+                    "https://www.luogu.com.cn/*",
+                    "https://www.luogu.me/*"
+                ]
+            }).then(tabs => {
+                for (const tab of tabs) {
+                    chrome.tabs.sendMessage(tab.id, data).catch(err => null);
+                }
+            }).catch(err => error(err));
+            break;
+        }
+        case "notify": {
+            const { title, msg } = data;
+            chrome.notifications.create({
+                type: "basic",
+                iconUrl: "assets/icon.png",
+                title: title,
+                message: msg,
+                priority: 1
+            });
+            break;
+        }
+        case "flush-cache":
+        case "clear-cache-physically": {
+            createOffscreen().then(() => {
+                chrome.runtime.sendMessage({ dst: "offscreen", type }).catch(err => null);
+            }, err => error(err));
+            break;
+        }
+        case "kill-offscreen": {
+            killOffscreen();
+            break;
+        }
+        default: {
+            error("Service Worker 无法识别的消息", message);
+            break;
+        }
+    }
+});
+
+chrome.webNavigation.onHistoryStateUpdated.addListener(details => {
+    const url = new URL(details.url);
+    if (url.hostname === "www.luogu.com.cn" || url.hostname === "www.luogu.me") {
+        chrome.tabs.sendMessage(details.tabId, { type: "route" }).catch(err => error(err));
+    }
+});
+
+console.log("Service Worker Loaded >w<");
---
+++ src/protocol_v2.js
@@ -0,0 +1,130 @@
+/*
+protocol_v2.js
+
+前端与 Service Worker 通信的模块。
+*/
+
+import { sleep } from "../public/lib/asyncio.js";
+import { error } from "../public/lib/core.js";
+
+export function ping() {
+    try {
+        const ret = chrome.runtime && chrome.runtime.id !== undefined;
+        if (!ret) emit("outdate");
+        return ret;
+    } catch (err) {
+        emit("outdate");
+        return false;
+    }
+}
+
+/** @param {() => Promise} fn */
+async function errorWrapper(fn) {
+    try {
+        return await fn();
+    } catch (err) {
+        if (ping()) throw err;
+        error(err, "已忽略此异常");
+        return null;
+    };
+}
+/**
+ * @param {Promise} promise
+ * @param {number} timeout
+ */
+async function timeoutWrapper(promise, timeout = 3000) {
+    return Promise.race([promise, sleep(timeout).then(() => null)]);
+}
+
+/**
+ * 返回的第一个是提交但未通过的选手，第二个是通过的选手
+ * @param {string} pid 
+ * @returns {Promise<{ passed: number[], submitted: number[] } | null>}
+ */
+async function acquireProblem(pid) {
+    return timeoutWrapper(errorWrapper(() => chrome.runtime.sendMessage({ dst: "sw", type: "query-pid", data: pid })));
+}
+
+/** @type {Map<number, LuoguProfileNew>} */
+const cachedProfiles = new Map();
+/**
+ * @param {number} uid 
+ * @returns {Promise<LuoguProfileNew | null>}
+ */
+async function acquireUserProfile(uid) {
+    if (cachedProfiles.has(uid)) return cachedProfiles.get(uid);
+    return timeoutWrapper(errorWrapper(() => chrome.runtime.sendMessage({ dst: "sw", type: "query-profile", data: uid }))).then(ret => {
+        if (ret?.uid) cachedProfiles.set(uid, ret);
+        return ret;
+    });
+}
+
+/** @returns {Promise<{ done: number, total: number } | null>} */
+async function acquireProgress() {
+    return timeoutWrapper(errorWrapper(() => chrome.runtime.sendMessage({ dst: "sw", type: "query-progress" })));
+}
+
+let cachedUID = null;
+let cachedUIDLastUpdate = 0;
+/** @returns {Promise<number | null>} */
+async function acquireUID() {
+    if (cachedUID !== null && cachedUIDLastUpdate + 60 * 1000 > Date.now()) return cachedUID;
+    return timeoutWrapper(errorWrapper(() => chrome.runtime.sendMessage({ dst: "sw", type: "query-uid" }))).then(ret => {
+        cachedUID = ret;
+        cachedUIDLastUpdate = Date.now();
+        return ret;
+    });
+}
+
+export function flushCache() {
+    cachedProfiles.clear();
+    errorWrapper(() => chrome.runtime.sendMessage({ dst: "sw", type: "flush-cache" }));
+}
+export function clearCache() {
+    cachedProfiles.clear();
+    errorWrapper(() => chrome.runtime.sendMessage({ dst: "sw", type: "clear-cache" }));
+}
+
+/** @type {Map<string, Set<(data: any) => void>>} */
+const subscription = new Map();
+/**
+ * @param {"problem" | "progress" | "route" | "outdate"} type
+ * @param {(data: any) => void} callback
+ */
+export function subscribe(type, callback) {
+    if (!subscription.has(type)) subscription.set(type, new Set());
+    const set = subscription.get(type);
+    set.add(callback);
+    return () => {
+        set.delete(callback);
+        if (!set.size) subscription.delete(type);
+    }
+}
+/** @param {"problem" | "progress" | "route" | "outdate"} type  */
+function emit(type, data) {
+    if (!subscription.has(type)) return;
+    for (const callback of subscription.get(type)) {
+        callback(data);
+    }
+}
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    const { type, data } = message;
+    switch (type) {
+        case "configured":
+            emit("problem", data);
+            break;
+        case "progress":
+            emit("progress", data);
+            break;
+        case "route":
+            emit("route", data);
+            break;
+        default: {
+            error("Protocol 无法识别的消息", message);
+            break;
+        }
+    }
+});
+
+export { acquireProblem, acquireUserProfile, acquireProgress, acquireUID }
