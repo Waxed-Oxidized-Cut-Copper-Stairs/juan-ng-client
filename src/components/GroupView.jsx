c3529d8072f8ad511f848024dad571cc55b79687
Wang-Yile <bluewindde@163.com>
2026-08-19 22:49:13 +0800

    feat(comp): GroupView 现在会统计打开完隐的人数

--- src/components/GroupView.jsx
+++ src/components/GroupView.jsx
@@ -4,16 +4,17 @@ import { useEffect, useMemo, useState } from "react";
 import AnimatedView, { Button, FloatDiv, FloatDivBinding, FloatDivContainer, percentToColor, percentToString, Username } from "./Generic";
 import styles from "./GroupView.module.css";
 
-import { acquireProblem, flushSpecificCache, subscribe } from "../protocol_v2";
+import { acquireProblem, acquireUserProfile, flushSpecificCache, subscribe } from "../protocol_v2";
 import StatusBar, { OnlineStatusBar } from "./StatusBar";
 
-export function QuickView({ ac, wa, tot, visible }) {
+export function QuickView({ ac, wa, na, tot, visible }) {
     return (
         <div style={{ display: "inline-block" }}>
             <AnimatedView visible={visible}>
                 <div className={styles.quickview}>
                     <span className={styles.ac}>{ac} </span>
                     <span className={styles.wa}>{wa} </span>
+                    <span className={styles.na}>{na} </span>
                     / {tot}
                     {/* <span style={{ color: percentToColor(Math.cbrt(ac / tot)) }}>{percentToString(ac, tot)}</span> */}
                 </div>
@@ -44,18 +45,20 @@ export function SingleView({ account, pid, state }) {
  * @param {string} [options.pid]
  * @param {Set<number>} [options.passed]
  * @param {Set<number>} [options.submitted]
+ * @param {Map<number, LuoguProfileNew>} [options.profiles]
  * @param {boolean} [options.verbose]
  */
-export function SingleGroupView({ group, pid, passed, submitted, verbose = false }) {
+export function SingleGroupView({ group, pid, passed, submitted, profiles, verbose = false }) {
     const [visible, setVisible] = useState(false);
-    const { cntAC, cntWA } = useMemo(() => {
-        let cntAC = 0, cntWA = 0;
-        for (const account of group.accounts) {
-            if (passed.has(account.luogu)) ++cntAC;
-            else if (submitted.has(account.luogu)) ++cntWA;
+    const { cntAC, cntWA, cntNA } = useMemo(() => {
+        let cntAC = 0, cntWA = 0, cntNA = 0;
+        for (const { luogu: uid } of group.accounts) {
+            if (passed.has(uid)) ++cntAC;
+            else if (submitted.has(uid)) ++cntWA;
+            else if (profiles.has(uid) && profiles.get(uid).privacy) ++cntNA;
         }
-        return { cntAC, cntWA };
-    }, [passed, submitted]);
+        return { cntAC, cntWA, cntNA };
+    }, [passed, submitted, profiles]);
     if (!(cntAC > 0 || cntWA > 0)) return null;
     const div = (
         <div>
@@ -84,19 +87,23 @@ export function SingleGroupView({ group, pid, passed, submitted, verbose = false
                                     <br />
                                     <strong className={styles.wa}>{cntWA}</strong>
                                     <br />
+                                    <strong className={styles.na}>{cntNA}</strong>
+                                    <br />
                                     <strong>{group.accounts.length}</strong>
                                 </div>
                                 <div className={styles.detailviewR}>
-                                    <span className={styles.emphasize}> Passed</span>
+                                    <span className={`${styles.emphasize} ${styles.ac}`}> Passed</span>
                                     <br />
-                                    <span className={styles.emphasize}> Submitted</span>
+                                    <span className={`${styles.emphasize} ${styles.wa}`}> Submitted</span>
+                                    <br />
+                                    <span className={`${styles.emphasize} ${styles.na}`}> Abstention</span>
                                     <br />
                                     <span className={styles.emphasize}> In Total</span>
                                 </div>
                             </div>
                             <div className={styles.percentview}>
                                 通过率
-                                <span className={styles.percentviewem} style={{ color: percentToColor(Math.cbrt(cntAC / group.accounts.length)) }}> {percentToString(cntAC, group.accounts.length)}</span>
+                                <span className={styles.percentviewem} style={{ color: percentToColor(Math.cbrt(cntAC / (group.accounts.length - cntNA))) }}> {percentToString(cntAC, group.accounts.length - cntNA)}</span>
                             </div>
                             <Button onClick={() => flushSpecificCache(group.accounts)}>刷新</Button>
                         </div>
@@ -136,6 +143,7 @@ export function SingleGroupView({ group, pid, passed, submitted, verbose = false
 export function GroupView({ groups, pid, children, verbose }) {
     const [passed, setPassed] = useState(new Set());
     const [submitted, setSubmitted] = useState(new Set());
+    const [profiles, setProfiles] = useState(new Map());
     let cancelled = false;
     async function update() {
         const situation = await acquireProblem(pid);
@@ -146,17 +154,36 @@ export function GroupView({ groups, pid, children, verbose }) {
         setPassed(new Set(situation.passed));
         setSubmitted(new Set(situation.submitted));
     }
+    async function updateProfile() {
+        const newProfiles = new Map();
+        const promises = [];
+        for (const group of groups) {
+            for (const account of group.accounts) {
+                const uid = account.luogu;
+                promises.push(acquireUserProfile(uid).then((profile) => {
+                    if (!profile) return;
+                    newProfiles.set(uid, profile);
+                }));
+            }
+        }
+        await Promise.allSettled(promises);
+        if (cancelled) return;
+        setProfiles(newProfiles);
+    }
     useEffect(() => {
         const abort = new AbortController();
         document.addEventListener("visibilitychange", () => update(), { signal: abort.signal });
-        const unload = subscribe("problem", () => update());
+        const unload1 = subscribe("problem", () => update());
+        const unload2 = subscribe("progress", () => updateProfile());
         return () => {
             abort.abort();
-            unload();
+            unload1();
+            unload2();
         }
     }, []);
     useEffect(() => {
         update();
+        updateProfile();
         return () => {
             cancelled = true;
         }
@@ -176,7 +203,7 @@ export function GroupView({ groups, pid, children, verbose }) {
             <div>
                 {groups.map((group, idx, arr) => {
                     return (
-                        <SingleGroupView key={idx} group={group} pid={pid} passed={passed} submitted={submitted} verbose={verbose} />
+                        <SingleGroupView key={idx} group={group} pid={pid} passed={passed} submitted={submitted} profiles={profiles} verbose={verbose} />
                     )
                 })}
             </div>
